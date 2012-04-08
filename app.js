78ad9b903fe11076e0d02b91744951b99effe175
mahata <nori@mahata.net>
2012-04-07 22:26:36 -0700

    modified to work on heroku.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -19,59 +18,58 @@ var app = module.exports = express.createServer();
 // Configuration
 // --
 app.configure(function(){
-                  app.set('views', __dirname + '/views');
-                  app.set('view engine', 'jade');
-                  app.use(express.bodyParser());
-                  app.use(express.methodOverride());
-                  app.use(app.router);
-                  app.use(express.static(__dirname + '/public'));
-              });
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
 app.configure('development', function(){
-                  express.logger('development mode');
-                  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-              });
+    express.logger('development mode');
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 app.configure('production', function(){
-                  express.logger('production mode');
-                  app.use(express.errorHandler());
-              });
+    express.logger('production mode');
+    app.use(express.errorHandler());
+});
 
 // --
 // Routes
 // --
 app.get('/', function(req, res){
-            var conf = JSON.parse(fs.readFileSync(__dirname + "/conf/conf.json", "utf-8"));
-            res.render('search', {
-                           title: 'Newgle',
-                           q: req.param("q"),
-                           conf: conf
-                       });
-        });
+    res.render('search', {
+        title: 'Newgle',
+        q: req.param("q")
+    });
+});
 app.get('/test', function(req, res){
-            console.log("add: " + calc.add(10, 2));
-            console.log("sub: " + calc.sub(10, 2));
-            console.log("mul: " + calc.mul(10, 2));
-            console.log("div: " + calc.div(10, 2));
+    console.log("add: " + calc.add(10, 2));
+    console.log("sub: " + calc.sub(10, 2));
+    console.log("mul: " + calc.mul(10, 2));
+    console.log("div: " + calc.div(10, 2));
 
-            res.send("Hi!");
-        });
+    res.send("Hi!");
+});
 app.get('/test2', function(req, res){
-            var _segmenter = new segmenter.TinySegmenter();
-            var segs = _segmenter.segment("私の名前は中野です");
-            res.send(segs.join(" | "));
-        });
+    var _segmenter = new segmenter.TinySegmenter();
+    var segs = _segmenter.segment("私の名前は中野です");
+    res.send(segs.join(" | "));
+});
 app.get('/api', function(req, res){
-            var params = {
-                q: req.param("q"),
-                p: req.param("p") ? req.param("p") : 1
-            };
-            bing.search(params, function(err, result) {
-                            res.setHeader("Content-Type", "application/json; charset=utf-8");
-                            res.send(result);
-                        });
-        });
+    var params = {
+        q: req.param("q"),
+        p: req.param("p") ? req.param("p") : 1
+    };
+    bing.search(params, function(err, result) {
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
+        res.send(result);
+    });
+});
 
 if (!module.parent) {
     app.listen(3000);
     console.log("Express server listening on port %d", app.address().port);
+    console.log(process.env.BING_SEARCH_HOST);
 }
 
