a6ef55a5f07dd6b47626b840b4921b443fb6bac4
mahata <nori@mahata.net>
2012-04-22 15:19:18 -0700

    modified the style for test codes, and added *vows* to package.json.

--- t/test-bing.js
+++ t/test-bing.js
@@ -3,26 +3,27 @@ var assert = require("assert");
 
 console.log("Bing Test Start.");
 
-vows.describe("Search Library").addBatch({"Bing Search": {
-                                                 topic: require(__dirname + "/../lib/bing"),
-                                                 "is a object": function(topic) {
-                                                     assert.equal("object", typeof topic);
-                                                 },
-                                                 "has a search function": function(topic) {
-                                                     assert.equal("function", typeof topic.search);
-                                                 },
-                                                 "in search function": {
-                                                     topic: function(bing) {
-                                                         var params = {
-                                                             q: "google",
-                                                             p: 1
-                                                         };
-                                                         bing.search(params, this.callback);
-                                                     },
-                                                     "there is search result": function(err, result) {
-                                                         var search_result = JSON.parse(result);
-                                                         assert.equal(0 < parseInt(search_result.SearchResponse.Web.Total), true);
-                                                  }
-                                              }
-                                          },
+vows.describe("Search Library").addBatch({
+    "Bing Search": {
+        topic: require(__dirname + "/../lib/bing"),
+        "is a object": function(topic) {
+            assert.equal("object", typeof topic);
+        },
+        "has a search function": function(topic) {
+            assert.equal("function", typeof topic.search);
+        },
+        "in search function": {
+            topic: function(bing) {
+                var params = {
+                    q: "google",
+                    p: 1
+                };
+                bing.search(params, this.callback);
+            },
+            "there is search result": function(err, result) {
+                var search_result = JSON.parse(result);
+                assert.equal(0 < parseInt(search_result.SearchResponse.Web.Total), true);
+            }
+        }
+    }
 }).export(module);
