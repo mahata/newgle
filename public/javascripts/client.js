08c03845b0c7e2cb50314b5265098c8a0d61a6bf
mahata <nori@mahata.net>
2012-04-22 12:53:16 -0700

    modified the behavior of 0 search result.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -18,71 +18,69 @@ client.search = function() {
         $("#search-summary").css("display","block");
         $("#bing-logo").css("display","block");
         $("#search-region").html("");
-
         $("#search-word").text($("#q").val());
         $("#search-hit-range-from").text(json.SearchResponse.Web.Offset + 1);
-        $("#search-hit-range-to").text(json.SearchResponse.Web.Offset + json.SearchResponse.Web.Results.length);
+        $("#search-hit-range-to").text(json.SearchResponse.Web.Offset +
+                                       ((undefined === json.SearchResponse.Web.Results) ? 0 : json.SearchResponse.Web.Results.length));
         $("#search-hit-num").text(
             json.SearchResponse.Web.Total.toString()
                 .replace(/([0-9]+?)(?=(?:[0-9]{3})+$)/g , "$1,"));
 
-        for (var i = 0; i < json.SearchResponse.Web.Results.length; i++) {
-            var deepLink = "";
-            if (json.SearchResponse.Web.Results[i].DeepLinks) {
-                var repeatNum = (json.SearchResponse.Web.Results[i].DeepLinks.length <= 5) ? json.SearchResponse.Web.Results[i].DeepLinks.length : 5,
+        if (undefined === json.SearchResponse.Web.Results) {
+            $('#search-region').append('<p>検索の結果、全くページが見つかりませんでした...。</p>');
+        } else {
+            for (var i = 0; i < json.SearchResponse.Web.Results.length; i++) {
+                var deepLink = "";
+                if (json.SearchResponse.Web.Results[i].DeepLinks) {
+                    var repeatNum = (json.SearchResponse.Web.Results[i].DeepLinks.length <= 5) ? json.SearchResponse.Web.Results[i].DeepLinks.length : 5,
                     linkList = [];
-                for (var j = 0; j < repeatNum; j++) {
-                    linkList.push('<a href="' + json.SearchResponse.Web.Results[i].DeepLinks[j].Url + '">' +
-                                   client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DeepLinks[j].Title, $("#q").val()) + '</a>');
+                    for (var j = 0; j < repeatNum; j++) {
+                        linkList.push('<a href="' + json.SearchResponse.Web.Results[i].DeepLinks[j].Url + '">' +
+                                      client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DeepLinks[j].Title, $("#q").val()) + '</a>');
+                    }
+                    deepLink = '<div class="search-result-deep-link"><strong>もっと見る:</strong> ' + linkList.join(" - ") + '</div>';
                 }
-                deepLink = '<div class="search-result-deep-link"><strong>もっと見る:</strong> ' + linkList.join(" - ") + '</div>';
+                $("#search-region").append('<li>' +
+                                           '<div class="search-result">' +
+                                           '<div class="search-result-left">' +
+                                           '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' +
+                                           '<img src="http://img.simpleapi.net/small/' + json.SearchResponse.Web.Results[i].Url + '" alt="" />' +
+                                           '</a>' +
+                                           '</div>' +
+                                           '<div class="search-result-right">' +
+                                           '<h2 class="search-result-title">' +
+                                           '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' + '<span id="search-result-title-' + i + '"></span>' + '</a>' +
+                                           '</h2>' +
+                                           '<p class="search-result-desc">' + '<span id="search-result-desc-' + i + '"></span>' + '</p>' +
+                                           '<div class="search-result-subtle-info">' +
+                                           '<span class="search-result-disp-url">' +
+                                           client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DisplayUrl, $("#q").val()) +
+                                           '</span>' +
+                                           ' - ' +
+                                           '<a class="search-result-cache" href="' + json.SearchResponse.Web.Results[i].CacheUrl + '">キャッシュ</a>' +
+                                           deepLink +
+                                           '<p style="clear: both;"></p>' +
+                                           '</div>' +
+                                           '</div>' +
+                                           '</div>' +
+                                           '</li>');
+                $("#search-result-title-" + i).text(json.SearchResponse.Web.Results[i].Title);
+                $("#search-result-desc-" + i).text(json.SearchResponse.Web.Results[i].Description);
+                // $("#search-result-title-" + i).html(json.SearchResponse.Web.Results[i].Title);
+                // $("#search-result-desc-" + i).html(json.SearchResponse.Web.Results[i].Description);
+                $("#search-result-desc-" + i).html(client.emphasizeKeyword($("#search-result-desc-" + i).html(), $("#q").val()));
             }
-            $("#search-region").append('<li>' +
-                                       '<div class="search-result">' +
-                                       '<div class="search-result-left">' +
-                                       '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' +
-                                       '<img src="http://img.simpleapi.net/small/' +
-                                       json.SearchResponse.Web.Results[i].Url +
-                                       '" alt="" />' +
-                                       '</a>' +
-                                       '</div>' +
-                                       '<div class="search-result-right">' +
-                                       '<h2 class="search-result-title">' +
-                                       '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' +
-                                       '<span id="search-result-title-' + i + '"></span>' +
-                                       '</a>' +
-                                       '</h2>' +
-                                       '<p class="search-result-desc">' +
-                                       '<span id="search-result-desc-' + i + '"></span>' +
-                                       '</p>' +
-                                       '<div class="search-result-subtle-info">' +
-                                       '<span class="search-result-disp-url">' +
-                                       client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DisplayUrl, $("#q").val()) +
-                                       '</span>' +
-                                       ' - ' +
-                                       '<a class="search-result-cache" href="' + json.SearchResponse.Web.Results[i].CacheUrl + '">キャッシュ</a>' +
-                                       deepLink +
-                                       '<p style="clear: both;"></p>' +
-                                       '</div>' +
-                                       '</div>' +
-                                       '</div>' +
-                                       '</li>');
-            $("#search-result-title-" + i).text(json.SearchResponse.Web.Results[i].Title);
-            $("#search-result-desc-" + i).text(json.SearchResponse.Web.Results[i].Description);
-            // $("#search-result-title-" + i).html(json.SearchResponse.Web.Results[i].Title);
-            // $("#search-result-desc-" + i).html(json.SearchResponse.Web.Results[i].Description);
-            $("#search-result-desc-" + i).html(client.emphasizeKeyword($("#search-result-desc-" + i).html(), $("#q").val()));
-        }
 
-        var pagingHtml = "";
-        if (1 < page) {
-            pagingHtml += "<a href=\"javascript:void(0);\" onclick=\"client.refresh(" + (parseInt(page) - 1) + "); return false;\">&laquo; 前の検索結果を見る</a>";
+            var pagingHtml = "";
+            if (1 < page) {
+                pagingHtml += "<a href=\"javascript:void(0);\" onclick=\"client.refresh(" + (parseInt(page) - 1) + "); return false;\">&laquo; 前の検索結果を見る</a>";
+            }
+            if (json.SearchResponse.Web.Offset + json.SearchResponse.Web.Results.length < json.SearchResponse.Web.Total) {
+                if ("" != pagingHtml) { pagingHtml += "&nbsp;|&nbsp;"; }
+                pagingHtml += "<a href=\"javascript:void(0);\" onclick=\"client.refresh(" + (parseInt(page) + 1) + "); return false;\">もっと検索結果を見る&raquo;</a>";
+            }
+            $("#search-pager").html(pagingHtml);
         }
-        if (json.SearchResponse.Web.Offset + json.SearchResponse.Web.Results.length < json.SearchResponse.Web.Total) {
-            if ("" != pagingHtml) { pagingHtml += "&nbsp;|&nbsp;"; }
-            pagingHtml += "<a href=\"javascript:void(0);\" onclick=\"client.refresh(" + (parseInt(page) + 1) + "); return false;\">もっと検索結果を見る&raquo;</a>";
-        }
-        $("#search-pager").html(pagingHtml);
 
         location.hash = "#q=" + $("#q").val() + "&p=" + page;
         $("#search-region").css("opacity", "1.0");
