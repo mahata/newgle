5a97f521dc500ae8c6f8140a834f547095dfebf0
mahata <nori@mahata.net>
2012-06-04 03:13:25 -0700

    modified to use EJS.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -28,47 +28,11 @@ client.search = function() {
         if (undefined === json.SearchResponse.Web.Results) {
             $('#search-region').append('<p>検索の結果、全くページが見つかりませんでした...。</p>');
         } else {
-            for (var i = 0; i < json.SearchResponse.Web.Results.length; i++) {
-                var deepLink = "";
-                if (json.SearchResponse.Web.Results[i].DeepLinks) {
-                    var repeatNum = (json.SearchResponse.Web.Results[i].DeepLinks.length <= 5) ? json.SearchResponse.Web.Results[i].DeepLinks.length : 5,
-                    linkList = [];
-                    for (var j = 0; j < repeatNum; j++) {
-                        linkList.push('<a href="' + json.SearchResponse.Web.Results[i].DeepLinks[j].Url + '">' +
-                                      client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DeepLinks[j].Title, $("#q").val()) + '</a>');
-                    }
-                    deepLink = '<div class="search-result-deep-link"><strong>もっと見る:</strong> ' + linkList.join(" - ") + '</div>';
-                }
-                $("#search-region").append('<li>' +
-                                           '<div class="search-result">' +
-                                           '<div class="search-result-left">' +
-                                           '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' +
-                                           '<img src="http://img.simpleapi.net/small/' + json.SearchResponse.Web.Results[i].Url + '" alt="" />' +
-                                           '</a>' +
-                                           '</div>' +
-                                           '<div class="search-result-right">' +
-                                           '<h2 class="search-result-title">' +
-                                           '<a href="' + json.SearchResponse.Web.Results[i].Url + '">' + '<span id="search-result-title-' + i + '"></span>' + '</a>' +
-                                           '</h2>' +
-                                           '<p class="search-result-desc">' + '<span id="search-result-desc-' + i + '"></span>' + '</p>' +
-                                           '<div class="search-result-subtle-info">' +
-                                           '<span class="search-result-disp-url">' +
-                                           client.emphasizeKeyword(json.SearchResponse.Web.Results[i].DisplayUrl, $("#q").val()) +
-                                           '</span>' +
-                                           ' - ' +
-                                           '<a class="search-result-cache" href="' + json.SearchResponse.Web.Results[i].CacheUrl + '">キャッシュ</a>' +
-                                           deepLink +
-                                           '<p style="clear: both;"></p>' +
-                                           '</div>' +
-                                           '</div>' +
-                                           '</div>' +
-                                           '</li>');
-                $("#search-result-title-" + i).text(json.SearchResponse.Web.Results[i].Title);
-                $("#search-result-desc-" + i).text(json.SearchResponse.Web.Results[i].Description);
-                // $("#search-result-title-" + i).html(json.SearchResponse.Web.Results[i].Title);
-                // $("#search-result-desc-" + i).html(json.SearchResponse.Web.Results[i].Description);
-                $("#search-result-desc-" + i).html(client.emphasizeKeyword($("#search-result-desc-" + i).html(), $("#q").val()));
-            }
+            var search_result = new EJS({url: "../templates/search.ejs"}).render(json.SearchResponse);
+            $("#search-region").append(search_result);
+            // $.each($('.search-result-desc'), function(i, desc) {
+            //     $('#search-result-desc-id-' + i).html(client.emphasizeKeyword($(desc).text(), $("#q").val()));
+            // });
 
             var pagingHtml = "";
             if (1 < page) {
@@ -102,17 +66,17 @@ client.getHashParams = function(locHash) {
     return urlParams;
 };
 
-// emphasize keyword from a text
-client.emphasizeKeyword = function(text, keyword) {
-    var keywordList = keyword.trim().replace(/[　\s]+/, " ").split(" ");
+// emphasize keyword from a text (will be removed)
+// client.emphasizeKeyword = function(text, keyword) {
+//     var keywordList = keyword.trim().replace(/[　\s]+/, " ").split(" ");
 
-    for (var i = 0; i < keywordList.length; i++) {
-        var reg = new RegExp("(" + keywordList[i] + ")", "gi");
-        text = text.replace(reg, "<strong>$1</strong>");
-    }
+//     for (var i = 0; i < keywordList.length; i++) {
+//         var reg = new RegExp("(" + keywordList[i] + ")", "gi");
+//         text = text.replace(reg, "<strong>$1</strong>");
+//     }
 
-    return text;
-};
+//     return text;
+// };
 
 $(function(){
     // for paging like: search result => search page using BACK button of browsers
